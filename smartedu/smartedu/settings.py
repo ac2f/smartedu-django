c56950dfe23d177215e7bfc45aeae3cd63f83d6f
ac2f <mustafabasarn3@gmail.com>
2022-05-17 23:01:07 +0300

    simple paths

--- smartedu/smartedu/settings.py
+++ smartedu/smartedu/settings.py
@@ -54,7 +54,9 @@ ROOT_URLCONF = 'smartedu.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / "templates"
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -115,7 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'templates/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
